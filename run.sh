70957656e8f9540e6e07464c17a7331369389df5
Archef2000 <69057532+Archef2000@users.noreply.github.com>
2022-11-20 19:17:58 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [ "${PROXY_HTTPS}" -eq "TRUE" ]; then
   sed -i "s\'https://' : 'http://'\'https://' : 'https://'\g" /var/www/vendor/athlon1600/php-proxy/src/helpers.php
+else
+  sed -i "s\'https://' : 'https://'\'https://' : 'http://'\g" /var/www/vendor/athlon1600/php-proxy/src/helpers.php
 fi
 service apache2 start
 tail -f /dev/null
